e0d041aadb829632608f6695e55d96cd8f79a09d
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2025-03-03 17:59:20 +0100

    Update item_details.dart

--- lib/pages/item_details.dart
+++ lib/pages/item_details.dart
@@ -4,103 +4,105 @@ import 'package:provider/provider.dart';
 import 'package:itemize/pages/edit_item.dart';
 import 'package:itemize/providers/room_data_provider.dart';
 
-class ItemDetailsPage extends StatefulWidget {
-  final Map<String, dynamic> item;
-  final VoidCallback onDelete;
+class ItemDetailsPage extends StatefulWidget { //StatefulWidget for dynamic updates
+  final Map<String, dynamic> item; //item data to show
+  final VoidCallback onDelete; //callback function to handle item deletion
 
-  const ItemDetailsPage({super.key, required this.item, required this.onDelete});
+  const ItemDetailsPage({super.key, required this.item, required this.onDelete}); //item data and onDelete callback are required parameters
 
   @override
-  _ItemDetailsPageState createState() => _ItemDetailsPageState();
+  _ItemDetailsPageState createState() => _ItemDetailsPageState(); //create state
 }
 
 class _ItemDetailsPageState extends State<ItemDetailsPage> {
-  late Map<String, dynamic> item;
+  late Map<String, dynamic> item; //store item details in a map
 
   @override
-  void initState() {
+  void initState() { //initialize the state
     super.initState();
-    item = widget.item;
+    item = widget.item; //copy the item data from widget's constructor
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //build the UI
     return Scaffold(
       appBar: AppBar(
-        title: Text('${item['name']} Details'),
+        title: Text('${item['name']} details'), //item's name as AppBar's title
       ),
-      body: Padding(
+      body: Padding( //add padding around the content
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column( //arrange content vertically
+          crossAxisAlignment: CrossAxisAlignment.start, //align widgets to the start of the column
           children: [
             Text(
-              item['name'],
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              item['name'], //item's name
+              style: const TextStyle( //text style
+                  fontSize: 24, //font size
+                  fontWeight: FontWeight.bold //bold font weight
+              ),
             ),
-            const SizedBox(height: 8),
-            Container(
-              width: 350,
-              height: 350,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8),
-                image: DecorationImage(
-                  // Modifica: se l'immagine inizia con "assets/", usa AssetImage, altrimenti FileImage
-                  image: item['image'].toString().startsWith('assets/')
-                      ? AssetImage(item['image'])
-                      : FileImage(File(item['image'])) as ImageProvider,
-                  fit: BoxFit.cover,
+            const SizedBox(height: 8), //add vertical space
+            Container( //container to show item's image
+              height: 350, //container height
+              width: 350, //container width
+              decoration: BoxDecoration( //set container decoration
+                border: Border.all(color: Colors.grey), //grey border
+                borderRadius: BorderRadius.circular(8), //round corners for the container
+                image: DecorationImage( //set image decoration
+                  image: item['image'].toString().startsWith('assets/') //check if image path starts with 'assets/'
+                      ? AssetImage(item['image']) //if the image path starts with 'assets/', load the image from assets folder
+                      : FileImage(File(item['image'])) as ImageProvider, //if the image path doesn't start with 'assets/', load the image from a file
+                  fit: BoxFit.cover, //ensure image covers the entire container
                 ),
               ),
             ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 8), //add vertical space
             Text(
-              'Category: ${item['category']}',
-              style: const TextStyle(fontSize: 16),
+              'Category: ${item['category']}', //item's category
+              style: const TextStyle(fontSize: 16), //font size
             ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 8), //add vertical space
             Text(
-              'Description: ${item['description']}',
-              style: const TextStyle(fontSize: 16),
+              'Description: ${item['description']}', //item's description
+              style: const TextStyle(fontSize: 16), //font size
             ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 8), //add vertical space
             Text(
-              'Room: ${item['room']}',
-              style: const TextStyle(fontSize: 16),
+              'Room: ${item['room']}', //item's room
+              style: const TextStyle(fontSize: 16), //font size
             ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            const SizedBox(height: 8), //add vertical space
+            Row( //arrange buttons horizontally
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, //distribute buttons evenly across the row
               children: [
-                SizedBox(
-                  width: 100,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      final updated = await Navigator.push(
+                SizedBox( //edit button container
+                  height: 50, //container height
+                  width: 100, //container width
+                  child: ElevatedButton( //ElevatedButton to edit the item
+                    onPressed: () async { //asynchronous function
+                      final updated = await Navigator.push( //push a new route
                         context,
                         MaterialPageRoute(
-                          builder: (context) => EditItemPage(item: item),
+                          builder: (context) => EditItemPage(item: item), //build EditItemPage widget
                         ),
                       );
-                      if (updated != null) {
-                        setState(() {
-                          item = updated;
+                      if (updated != null) { //if changes were made
+                        setState(() { //update UI state
+                          item = updated; //update item data
                         });
                       }
                     },
-                    child: const Text('Edit'),
+                    child: const Text('Edit'), //edit button's text
                   ),
                 ),
-                SizedBox(
-                  width: 100,
-                  height: 50,
-                  child: ElevatedButton(
+                SizedBox( //delete button container
+                  height: 50, //container height
+                  width: 100, //container width
+                  child: ElevatedButton( //ElevatedButton to delete the item
                     onPressed: () {
-                      _showDeleteConfirmation(context);
+                      _showDeleteConfirmation(context); //show delete confirmation dialog
                     },
-                    child: const Text('Delete'),
+                    child: const Text('Delete'), //delete button's text
                   ),
                 ),
               ],
@@ -111,28 +113,28 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
     );
   }
 
-  void _showDeleteConfirmation(BuildContext context) {
-    showDialog(
+  void _showDeleteConfirmation(BuildContext context) { //show delete confirmation dialog
+    showDialog( //open dialog box
       context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Delete Item'),
-          content: const Text('Are you sure you want to delete this item?'),
+      builder: (context) { //create dialog's content
+        return AlertDialog( //create AlertDialog box
+          title: const Text('Delete item'), //dialog's title
+          content: const Text('Are you sure you want to delete this item?'), //dialog's message
           actions: [
-            TextButton(
+            TextButton( //confirm deletion button
               onPressed: () {
-                context.read<RoomDataProvider>().deleteItem(item['room'], item['name']);
-                widget.onDelete();
-                Navigator.pop(context); // Chiude il dialog
-                Navigator.pop(context); // Torna indietro dopo la cancellazione
+                context.read<RoomDataProvider>().deleteItem(item['room'], item['name']); //delete item from RoomDataProvider
+                widget.onDelete(); //calls onDelete callback provided to the ItemDetailsPage widget
+                Navigator.pop(context); //close the dialog
+                Navigator.pop(context); //close ItemDetailsPage
               },
-              child: const Text('Delete'),
+              child: const Text('Delete'), //confirm deletion button's text
             ),
-            TextButton(
+            TextButton( //cancel deletion button
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context); //close the AlertDialog
               },
-              child: const Text('Cancel'),
+              child: const Text('Cancel'), //cancel deletion button's text
             ),
           ],
         );
